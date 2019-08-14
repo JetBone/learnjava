2d03306c1b11ef7803b0df463bbbb554ff7a70e4
Chris <chris3chan@gmail.com>
2019-08-14 22:27:47 +0800

    singleton

--- src/main/java/com/jetbone/design/patterns/creational/singleton/ThreadTest.java
+++ src/main/java/com/jetbone/design/patterns/creational/singleton/ThreadTest.java
@@ -6,7 +6,17 @@ package com.jetbone.design.patterns.creational.singleton;
 public class ThreadTest implements Runnable {
     @Override
     public void run() {
+        // 懒汉式单例模式，多线程测试
         LazySingleton lazySingleton = LazySingleton.getInstance();
         System.out.println(Thread.currentThread().getName() + ": " + lazySingleton);
+
+        // 容器单例模式，多线程测试
+        ContainerSingleton.setInstance("JetBone", new Object());
+        Object containerSingleton = ContainerSingleton.getInstance("JetBone");
+        System.out.println(Thread.currentThread().getName() + ": " + containerSingleton);
+
+        // ThreadLocal单例模式，多线程测试
+        ThreadLocalSingleton threadLocalSingleton = ThreadLocalSingleton.getInstance();
+        System.out.println(Thread.currentThread().getName() + ": " + threadLocalSingleton);
     }
 }
