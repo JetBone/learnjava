2e92300e8962130e74d7e0b2ad9b9de9bd0f17d0
Chris <chris3chan@gmail.com>
2019-10-10 15:01:55 +0800

    inherit

---
+++ src/main/java/com/jetbone/inherit/Father1.java
@@ -0,0 +1,8 @@
+package com.jetbone.inherit;
+
+/**
+ * Created by Chris on 2019/10/10
+ */
+public class Father1 {
+    private String name = "father";
+}
---
+++ src/main/java/com/jetbone/inherit/Son1.java
@@ -0,0 +1,8 @@
+package com.jetbone.inherit;
+
+/**
+ * Created by Chris on 2019/10/10
+ */
+public class Son1 extends Father1 {
+    public String name = "son";
+}
