2b03e3e868abcde1283e224c0af61646572686a9
Chris <chris3chan@gmail.com>
2020-04-09 09:44:20 +0800

    pdf

---
+++ src/main/java/com/jetbone/concurrent/MyBlockingQueue.java
@@ -0,0 +1,9 @@
+package com.jetbone.concurrent;
+
+/**
+ * Created by Chris on 2020/4/7
+ */
+public class MyBlockingQueue {
+
+
+}
--- src/main/java/com/jetbone/pdf/PDFDemo.java
+++ src/main/java/com/jetbone/pdf/PDFDemo.java
@@ -1,8 +1,7 @@
 package com.jetbone.pdf;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.*;
 
 import javax.print.Doc;
 import java.io.File;
@@ -16,17 +15,80 @@ public class PDFDemo {
 
     public static void main(String[] args) throws Exception {
 
-//        File file = new File("./demo.pdf");
+        Document document = new Document(PageSize.A4);
+        Rectangle rect = new Rectangle(40, 806, 60, 788);
 
-//        Document document = new Document();
-//
-//        PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream("./demoPdf.pdf"));
-//
-//        document.open();
-//
+        BaseFont bfChinese = BaseFont.createFont( "STSongStd-Light" ,"UniGB-UCS2-H",BaseFont.NOT_EMBEDDED);
+        Font font = new Font(bfChinese, 12,Font.NORMAL);
+
+
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream("./demoPdf.pdf"));
+
+//        PdfFormField form = PdfFormField.createEmpty(pdfWriter);
+        document.open();
+
+
+//        TextField tf = new TextField(pdfWriter, rect, "function");
+//        tf.setBorderColor(BaseColor.BLACK);
+//        tf.setOptions(TextField.READ_ONLY | TextField.MULTILINE);
+////        tf.setBorderWidth(2);
+////        tf.setTextColor(BaseColor.RED);
+//        tf.setFontSize(12);
+//        tf.setText("Text field");
+//        pdfWriter.addAnnotation(tf.getTextField());
+
+        Paragraph paragraph1 = new Paragraph("function: ");
+        paragraph1.add(new Chunk("value1"));
+        paragraph1.add(new Chunk("            "));
+        paragraph1.add(new Chunk("Category: "));
+        paragraph1.add(new Chunk("value2"));
+        paragraph1.add(new Chunk("            "));
+        paragraph1.add(new Chunk("Scope: "));
+        paragraph1.add(new Chunk("中文", font));
+
+        Paragraph paragraph2 = new Paragraph("function: ");
+        paragraph2.add(new Chunk("value1"));
+        paragraph2.add(new Chunk("            "));
+        paragraph2.add(new Chunk("Category: "));
+        paragraph2.add(new Chunk("value2"));
+        paragraph2.add(new Chunk("            "));
+        paragraph2.add(new Chunk("Scope: "));
+        paragraph2.add(new Chunk("value3"));
+
+        document.add(paragraph1);
+        document.add(paragraph2);
+        document.add(new Paragraph("   "));
+
+        PdfPTable table = new PdfPTable(6);
+
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+        table.addCell("123");
+
+        document.add(table);
+
+
+
 //        document.add(new Paragraph("Hello world"));
-//
-//        document.close();
+//        document.add(new Paragraph("Paragraph1"));
+//        document.add(new Paragraph("Paragraph2"));
+
+        document.close();
 
     }
 
