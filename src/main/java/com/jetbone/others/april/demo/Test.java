96fcf28d8cf6e004e58ddde83cbac29ca06e97fd
Chris <chris3chan@gmail.com>
2021-07-03 09:13:10 +0800

    test

--- src/main/java/com/jetbone/others/april/demo/Test.java
+++ src/main/java/com/jetbone/others/april/demo/Test.java
@@ -1,14 +1,43 @@
 package com.jetbone.others.april.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Chris on 2019/3/21.
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.out.println(((TestInterface)(a, b) -> a+b).func(1, 2));
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonTest jsonTest = new JsonTest();
+        jsonTest.setCode("1");
+        jsonTest.setName("2");
+        List<JsonTest> list = new ArrayList<>();
+        list.add(jsonTest);
+        var result = objectMapper.writeValueAsString(list);
+        System.out.println(result);
+
+
+    }
+
+    @Data
+    static class JsonTest {
+        private String code;
+
+        private String name;
+
+        private String value;
+
+        @JsonIgnore
+        private int index;
     }
 
 }
